6629a70004f763d174b2bcef2cdd5cbcd915d9b3
Benjamin Figeiredo <bfigeiredo@tradespark.la>
2023-12-10 13:46:15 -0300

    Added info and improve UI

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Education from "./components/Education";
 
 export default function App() {
   return (<main className="text-gray-400 bg-gray-900 body-font">
     <Navbar />
     <About />
+    <Education />
     <Projects />
     <Skills />
     <Contact />
--- src/data.js
+++ src/data.js
@@ -1,43 +1,54 @@
 export const projects = [
   {
-    title: "React Reserve",
-    subtitle: "MERN Stack",
+    title: "TradeSpark", 
+    subtitle: "Semisenior Fullstack Developer & Project Support (March 2022 - present)",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-1.gif",
-    link: "https://reactbootcamp.com",
+      "I work developing features/improvements pertaining to the backend and frontend of the TradeSpark website. I participate in the design and modeling of the platform architecture, which follows a microservices architecture. At the same time, I am project support, so I address the problems that clients have related to the software, offering solutions according to their needs.",
+    image: "./project-1.png",
+    link: "https://tradespark.la/",
   },
   {
-    title: "React Tracks",
-    subtitle: "React and Python",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-2.gif",
-    link: "https://reactbootcamp.com",
+    title: "EVC-CIN scholarship", 
+    subtitle: "Intern (June 2021 - June 2022)", 
+    description: "I worked in collaboration with INRIA (France) in the development of a deep learning model capable of detecting security vulnerabilities through the analysis of JavaScript source code.",
+    image: "./project-2.png",
+    link: "https://evc.cin.edu.ar/",
   },
   {
-    title: "DevChat",
-    subtitle: "React and Firebase",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-3.gif",
-    link: "https://reactbootcamp.com",
-  },
-  {
-    title: "Epic Todo App",
-    subtitle: "React Hooks",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-4.gif",
-    link: "https://reactbootcamp.com",
-  },
+    title: "UNICEN", 
+    subtitle: "Assistant Professor (March 2021 - March 2022)",   
+    description: "I worked as an assistant professor in the subject of Software Methodologies, in which I taught the fundamentals of Scrum and Agile Methodologies, as well as the dynamics of teamwork in software.",
+    image: "./project-3.jpeg",
+    link: "https://exa.unicen.edu.ar/",
+  }
 ];
 
 export const skills = [
-  "JavaScript",
-  "React",
-  "Next.js",
-  "Node",
-  "GraphQL",
-  "Material UI",
+  "Object Oriented Programming",
+  "Python",
+  "Django",
+  "Golang",
+  "Relational databases (MySQL)",
+  "NoSQL databases (redis, elasticsearch)",
+  "JavaScript/TypeScript",
+  "HTML/CSS",
+  "Angular",
+  "Docker"
+];
+
+export const education = [
+  {
+    title: "Software Engineering (2017 - 2023)", 
+    subtitle: "Universidad Nacional del Centro de la Provincia de Buenos Aires", 
+    description: "5-year bachelor's degree. Finished. ",
+    image: "./education-1.jpeg",
+    link: "https://exa.unicen.edu.ar/",
+  },
+  {
+    title: "Software Analyst (2017 - 2021)", 
+    subtitle: "Universidad Nacional del Centro de la Provincia de Buenos Aires", 
+    description: "Intermediate degree from Software Engineering. Finished.",
+    image: "./education-1.jpeg",
+    link: "https://exa.unicen.edu.ar/",
+  },
 ];
